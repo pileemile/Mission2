d6453dfae55d4e67fed06bd12d534508847d97e3
PICARD Emile <emilepicard323@gmail.com>
2023-12-13 18:03:20 +0100

    main

--- PHP/includes/insertcatalogue.php
+++ PHP/includes/insertcatalogue.php
@@ -1,28 +1,23 @@
 <?php
-    include("connectionbdd.php");
-    session_start();
-        $libelleDomaine = $_SESSION['domaine'];
-        var_dump($libelleDomaine);
-        header("Location: ../catalogue.php" );
-        if (isset($_SESSION['domaine'])){
-            $libelle = $_POST['formation'];
-            $date = $_POST['date'];
-            $horaire = $_POST['horaire'];
-            //$lieu = $_POST['lieu'];
-            $intervenant = $_POST['intervenant'];
-            $public = $_POST['public'];
-            $objectifs = $_POST['objectifs'];
-            $contenu = $_POST['contenu'];
-            $cout = $_POST['cout'];
-            //$dateLimite = $_POST['dateLimite'];
-            $idDomaine = $_POST['domaine'];
+session_start();
+
+// Vérifiez si le formulaire a été soumis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupérez les données du formulaire
+    $domaine = $_POST["gestion"] ?? $_POST["informatique"] ?? $_POST["developpement"] ?? $_POST["Secourisme"] ?? $_POST["Communication"] ?? "";
+
+    $formation = $_POST[$domaine] ?? "";
+
+    $date = $_POST["date"] ?? "";
+    $horaire = $_POST["horaire"] ?? "";
+    $lieu = $_POST["lieu"] ?? "";
+    $intervenant = $_POST["intervenant"] ?? "";
+    $public = $_POST["public"] ?? "";
+    $objectifs = $_POST["objectifs"] ?? "";
+    $contenu = $_POST["contenu"] ?? "";
+    $cout = $_POST["cout"] ?? "";
+    $dateLimite = $_POST["dateLimite"] ?? "";
+
     
-            //$reqSQL = "INSERT INTO formation (`idFormation`, `Libelle`, `DateFormation`, `Horaire`, `Intervenant`, `Public`, `Objectifs`, `Contenu`, `coût`, `idDomaine`) VALUES (NULL, '$libelle', '$date', '$horaire', '$intervenant', '$public', '$objectifs', '$contenu', '$cout','$idDomaine')";
-    
-            //$connexion->exec($reqSQL);
-            //echo "Insertion réussie.";
-        }
-       
-    
-    
+}
 ?>
